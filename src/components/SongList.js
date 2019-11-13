6324b7f88f2e1e77741a495a0cf6aef71e65210d
pavodev <pavicivan.ip@gmail.com>
2019-11-13 17:11:29 +0100

    implemented redux connect

---
+++ src/components/SongList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class SongList extends React.Component {
+  render() {
+    return <div>SongList</div>;
+  }
+}
+
+const mapStateToProps = state => {
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
+
+/* 
+    function connect() {
+        return function(){
+            return 'Hi there!';
+        }
+    }
+
+    connect()(); // automatically invokes the returned function
+*/
